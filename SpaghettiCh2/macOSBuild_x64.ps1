7b6ce5228df5b73ff7accb8a5949b055df9926e6
Jacob Arbib <jacob.arbib@gmail.com>
2025-08-01 11:39:47 +0100

    updated release profiles directories

--- SpaghettiCh2/macOSBuild_x64.ps1
+++ SpaghettiCh2/macOSBuild_x64.ps1
@@ -3,5 +3,5 @@
 chcp 65001
 cd .\SpaghettiCh2\
 dotnet restore -r osx-x64
-dotnet msbuild -t:BundleApp -p:RuntimeIdentifier=osx-x64 -p:Configuration=Release -p:UseAppHost=true -p:PublishSingleFile=False -p:PublishTrimmed=False -p:SelfContained=true -p:PublishDir=bin/Release/net8.0/publish/USPMac
+dotnet msbuild -t:BundleApp -p:RuntimeIdentifier=osx-x64 -p:Configuration=Release -p:UseAppHost=true -p:PublishSingleFile=False -p:PublishTrimmed=False -p:SelfContained=true -p:PublishDir=bin/Release/net8.0/publish/osx-x64/
 cd ..
