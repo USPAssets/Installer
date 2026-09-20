a1694b89b3dd2c427749c43d66195bc87ad3a182
renard <jacob.arbib@gmail.com>
2026-09-20 16:54:55 +0200

    Updated python script and deleted old ps scripts

--- SpaghettiCh2/macOSBundleApp.py
+++ SpaghettiCh2/macOSBundleApp.py
@@ -3,7 +3,7 @@ import subprocess
 import shutil
 
 def main():
-    selection = input("Bundle x64 (1) or arm64 (2)?")
+    selection = input("Bundle x64 (1) or arm64 (2)? \n")
     if not selection.isdigit() or int(selection) < 1 or int(selection) > 2:
         raise ValueError("Insert either 1 or 2 to select the architecture.")
 
@@ -11,6 +11,23 @@ def main():
     arch = "osx-x64" if parsed == 1 else "osx-arm64"
     current_path = Path(__file__).parent.resolve()
     proj_path = current_path / "SpaghettiCh2" / "USPInstaller.csproj"
+    plist_path = current_path / "Info.plist"
+    icon_path = current_path / "SpaghettiCh2" / "USPInstaller.icns"
+    
+    app_path = current_path / "publish" / arch / "USPInstaller.app"
+    if app_path.exists():
+        shutil.rmtree(app_path)
+
+    app_path.mkdir(parents=True)
+    
+    contents = app_path / "Contents"
+    contents.mkdir()
+
+    resources = contents / "Resources"
+    resources.mkdir()
+
+    macOSFolder = contents / "MacOS"
+    macOSFolder.mkdir()
 
     BUILD_COMMAND = [
         "dotnet", 
@@ -23,29 +40,10 @@ def main():
         '-p:PublishReadyToRun=false', 
         '-p:PublishTrimmed=false', 
         '--self-contained', 
-        '-o'
+        '-o',
+        str(macOSFolder)
     ]
 
-    plist_path = current_path / "Info.plist"
-    icon_path = current_path / "SpaghettiCh2" / "USPInstaller.icns"
-    
-    app_patb = current_path / "USPInstaller.app"
-    if app_patb.exists():
-        shutil.rmtree(app_patb)
-
-    app_patb.mkdir()
-    
-    contents = app_patb / "Contents"
-    contents.mkdir()
-
-    resources = contents / "Resources"
-    resources.mkdir()
-
-    macOSFolder = contents / "MacOS"
-    macOSFolder.mkdir()
-
-    folder = f'{macOSFolder}'
-    BUILD_COMMAND.append(folder)
     subprocess.run(BUILD_COMMAND)
     shutil.copy2(plist_path, contents)
     shutil.copy2(icon_path, resources)
